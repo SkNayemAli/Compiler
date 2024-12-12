0ff19b926e3d9741c42f3739062734ab084da3d1
Nayem <sknayemali2023@gmail.com>
2024-12-12 20:25:39 +0530

    Fix line endings in Api.js

--- Api.js
+++ Api.js
@@ -1,39 +1,42 @@
-const express = require("express")
-const app = express()
-const bodyp = require("body-parser")
-const compiler = require("compilex")
-const options = { stats: true }
-compiler.init(options)
+const express = require("express");
+const app = express();
+const bodyp = require("body-parser");
+const compiler = require("compilex");
+const options = { stats: true };
+compiler.init(options);
 
-app.use(bodyp.json())
+// Middleware for parsing JSON bodies
+app.use(bodyp.json());
 
-app.use("/codemirror-5.65.16", express.static("D:/WEB DEVOLOPMENT/MY OWN PROJECT/Compiler/codemirror-5.65.16"))
-app.use("/codemirror-5.65.16", express.static(__dirname + "/codemirror-5.65.16"))
+// Serve static files (like CSS, JS, Images) from the public directory
+app.use(express.static("public")); // Make sure your style.css and codemirror-5.65.16 are inside the "public" folder
 
+// Serve the main page
 app.get("/", function (req, res) {
     compiler.flush(function () {
-        console.log("deleted")
-    })
-    res.sendFile("D:/WEB DEVOLOPMENT/MY OWN PROJECT/Compiler/index.html")
-})
+        console.log("deleted");
+    });
+    res.sendFile(__dirname + "/index.html"); // Ensure your index.html is in the root directory
+});
 
+// Handle compile requests
 app.post("/compile", function (req, res) {
-    var code = req.body.code
-    var input = req.body.input
-    var lang = req.body.lang
+    var code = req.body.code;
+    var input = req.body.input;
+    var lang = req.body.lang;
+
     try {
         if (lang == "C_&_Cpp") {
+            const envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
             if (!input) {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
                 compiler.compileCPP(envData, code, function (data) {
                     if (data.output) {
                         res.send(data);
                     } else {
-                        res.send({ output: "error" })
+                        res.send({ output: "error" });
                     }
                 });
             } else {
-                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } };
                 compiler.compileCPPWithInput(envData, code, input, function (data) {
                     if (data.output) {
                         res.send(data);
@@ -42,51 +45,52 @@ app.post("/compile", function (req, res) {
             }
         }
         else if (lang == "Java") {
+            const envData = { OS: "windows" };
             if (!input) {
-                var envData = { OS: "windows" };
                 compiler.compileJava(envData, code, function (data) {
                     if (data.output) {
                         res.send(data);
                     } else {
-                        res.send({ output: "error" })
+                        res.send({ output: "error" });
                     }
                 });
             } else {
-                var envData = { OS: "windows" };
                 compiler.compileJavaWithInput(envData, code, input, function (data) {
                     if (data.output) {
                         res.send(data);
                     } else {
-                        res.send({ output: "error" })
+                        res.send({ output: "error" });
                     }
                 });
             }
         }
         else if (lang == "Python") {
+            const envData = { OS: "windows" };
             if (!input) {
-                var envData = { OS: "windows" };
                 compiler.compilePython(envData, code, function (data) {
                     if (data.output) {
                         res.send(data);
                     } else {
-                        res.send({ output: "error" })
+                        res.send({ output: "error" });
                     }
                 });
             } else {
-                var envData = { OS: "windows" };
                 compiler.compilePythonWithInput(envData, code, input, function (data) {
                     if (data.output) {
                         res.send(data);
                     } else {
-                        res.send({ output: "error" })
+                        res.send({ output: "error" });
                     }
                 });
             }
         }
+    } catch (e) {
+        console.log("error", e);
+        res.send({ output: "error" });
     }
-    catch (e) {
-        console.log("error")
-    }
-})
+});
 
-app.listen(8000);
+// Start server
+app.listen(8000, () => {
+    console.log("Server is running on http://localhost:8000");
+});
